f5be3be32093c9424c6fb125e6ef9c882fa511fd
@hexlet/cli <support@hexlet.io>
2024-03-24 16:28:42 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -1,8 +1,5 @@
 package exercise;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
