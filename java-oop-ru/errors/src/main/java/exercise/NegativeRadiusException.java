7228abbd06b82ce8a1aabd0217f821e1ef5fbc42
@hexlet/cli <support@hexlet.io>
2024-03-28 13:01:36 +0300

    submit java-oop-ru/errors

--- java-oop-ru/errors/src/main/java/exercise/NegativeRadiusException.java
+++ java-oop-ru/errors/src/main/java/exercise/NegativeRadiusException.java
@@ -2,14 +2,7 @@ package exercise;
 
 // BEGIN
 public class NegativeRadiusException extends Exception {
-    /**
-     * Constructs a new exception with the specified detail message.  The
-     * cause is not initialized, and may subsequently be initialized by
-     * a call to {@link #initCause}.
-     *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
-     */
+    public NegativeRadiusException() { }
     public NegativeRadiusException(String message) {
         super(message);
     }
