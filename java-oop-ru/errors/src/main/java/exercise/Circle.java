13aa64dce4807543854261e5f158b0b3b7698c3c
@hexlet/cli <support@hexlet.io>
2024-03-28 13:18:10 +0300

    submit java-oop-ru/errors

--- java-oop-ru/errors/src/main/java/exercise/Circle.java
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -17,7 +17,7 @@ public class Circle {
         if (radius < 0) {
             throw new NegativeRadiusException("Radius:" + radius + "cannot be negative");
         }
-        return double = Math.PI * (radius * radius);
+        return Math.PI * (radius * radius);
     }
 
 
